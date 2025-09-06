418d52b45fe8e2ef1d044fa4bb90d3aaeb76782b
oshik <kihsogaming@gmail.com>
2025-09-06 23:42:59 +0545

    dlt options

--- Server/server.js
+++ Server/server.js
@@ -29,15 +29,7 @@ app.use(
 );
 
 // Optional: make sure OPTIONS is handled
-app.options(
-  "/{*any}",
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+
 const port = process.env.PORT || 3000;
 app.use("/", router);
 app.use("/", router2);
