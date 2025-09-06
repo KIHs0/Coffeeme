d66f59527d4f1225f1a2c6ddd5104954c43ad228
oshik <kihsogaming@gmail.com>
2025-09-06 23:38:47 +0545

    cors

--- Client/src/utils/axiosInstance.js
+++ Client/src/utils/axiosInstance.js
@@ -4,5 +4,5 @@ const DB_URL = import.meta.env.VITE_BASE_URL;
 export const axiosInstance = axios.create({
   baseURL: DB_URL,
   withCredentials: true,
-  headers: { ContentType: "application/json" },
+  headers: { "Content-Type": "application/json" },
 });
