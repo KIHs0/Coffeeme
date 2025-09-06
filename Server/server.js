f7a51cc8c71d47e9d2b006f342d915c9c5d74866
oshik <kihsogaming@gmail.com>
2025-09-06 23:47:51 +0545

    cors fix 2

--- Server/server.js
+++ Server/server.js
@@ -15,13 +15,7 @@ console.log(allowedOrigins);
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: process.env.CLIENT_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
