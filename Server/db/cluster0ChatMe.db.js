e4c4a73684cb3dd510ca9097f79d57c7f36abdb7
oshik <kihsogaming@gmail.com>
2025-09-08 22:57:59 +0545

    fix bug

--- Server/db/cluster0ChatMe.db.js
+++ Server/db/cluster0ChatMe.db.js
@@ -2,5 +2,12 @@ import mongoose from "mongoose";
 
 export const connection1 = async function main() {
   const MONGO_URL = process.env.MONGO_URL;
-  await mongoose.connect(MONGO_URL).then((res) => {});
+  await mongoose
+    .connect(MONGO_URL)
+    .then((res) => {
+      console.log("connected");
+    })
+    .catch((err) => {
+      console.log("cant connect");
+    });
 };
