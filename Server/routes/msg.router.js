3ac944c8d9615c17b4403b1ae99b3a94cc044f94
oshik <kihsogaming@gmail.com>
2025-09-16 21:58:41 +0545

    rm unnecessary code callthunk

--- Server/routes/msg.router.js
+++ Server/routes/msg.router.js
@@ -1,8 +1,7 @@
 import express from "express";
 import { IsAuthenticated } from "../middleware/isAuthenticate.js";
 export const router2 = express.Router();
-import { sendMsg, getMsg, callMsg } from "../controller/msg.controller.js";
+import { sendMsg, getMsg } from "../controller/msg.controller.js";
 
 router2.post("/send/:receiverId", IsAuthenticated, sendMsg);
 router2.post("/get/:receiverId", IsAuthenticated, getMsg);
-router2.post("/call/:receiverId", IsAuthenticated, callMsg);
